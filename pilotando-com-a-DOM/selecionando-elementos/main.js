912c913c14f64fb5db97d6e9ba6c3c080b0e689e
CristianPietro <crpietro26@gmail.com>
2022-11-02 10:05:29 -0300

    file deleted

--- pilotando-com-a-DOM/selecionando-elementos/main.js
+++
@@ -1,8 +0,0 @@
-const titulo = document.getElementById('title')
-console.log(titulo)
-
-const texto = document.getElementsByClassName('text')
-console.log(texto)
-
-const secao = document.getElementsByTagName('section')
-console.log(secao)
