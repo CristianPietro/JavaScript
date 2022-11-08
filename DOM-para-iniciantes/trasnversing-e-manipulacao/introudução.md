8630a07fa9035e658c042c6dc6ea4cf0d4be0287
CristianPietro <crpietro26@gmail.com>
2022-11-08 14:17:41 -0300

    transversing e manipulacao

---
+++ DOM-para-iniciantes/trasnversing-e-manipulacao/introudução.md
@@ -0,0 +1,11 @@
+# outerHTML, innerHTML, innerTEXT
+
+- Propriedades que retornam  uma striing contendo o html ou texto.
+- É possível atribuir um valor para as mesmas
+- `` element.innerText = 'novo texto' `` 
+
+## Transversing
+
+- Como navegar pelo DOM, ultilizando suas proprias propriedades e metodos
+
+ 
---
+++ DOM-para-iniciantes/trasnversing-e-manipulacao/script.js
@@ -0,0 +1,24 @@
+/***
+ * outerHTML, innerHTML, innerText
+ */
+
+const menu = document.querySelector("h1");
+
+const animaisDescrition = document.querySelector('.animais-descricao')
+
+//console.log(animaisDescrition.outerHTML)
+//console.log(animaisDescrition.innerHTML)
+//console.log(animaisDescrition.innerText)
+
+
+
+/**
+ * # TRASNVERSING E MANIPULAÇÃO
+ */
+
+
+const lista = document.querySelector(".animais-lista")
+
+
+console.log(lista)
+  
